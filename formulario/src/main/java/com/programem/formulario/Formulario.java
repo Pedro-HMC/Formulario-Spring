ac91ef0126b487315ba61b46184fd13eea016964
Pedro Henrique Medeiros Câmara <pedro.henrique.camara2002@gmail.com>
2020-07-16 16:03:03 -0300

    Fórmularios funcionando

---
+++ formulario/src/main/java/com/programem/formulario/Formulario.java
@@ -0,0 +1,23 @@
+package com.programem.formulario;
+
+public class Formulario {
+    private String nome;
+    private String idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getIdade(){
+        return this.idade;
+    }
+
+    public void setIdade(String idade){
+        this.idade = idade;
+    }
+
+}
